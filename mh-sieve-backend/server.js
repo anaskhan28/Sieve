599eba67489051b779b9d1890a6132429be42921
Deep Bansode <drasticoder@gmail.com>
2023-12-16 14:30:08 +0530

    init

---
+++ mh-sieve-backend/server.js
@@ -0,0 +1,14 @@
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
+
+(async () => {
+  try {
+    // Connect to the db
+    await connectDB();
+
+    const port = process.env.PORT || 8000;
+    const server = app.listen(port, () => {
+      console.log(`Server is running on ${port}`);
+    });
+
+ })();
